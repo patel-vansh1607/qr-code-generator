beaa592316647c2d7ab951e5522149ddc1560f30
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-24 22:30:15 +0300

    Refactor App.js to improve code readability and maintainability; set default background color and update class names for consistency.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import './App.css';
 
@@ -6,28 +5,36 @@ function App() {
   const [temp, setTemp] = useState("");
   const [word, setWord] = useState("");
   const [size, setSize] = useState(400);
-  const [bgColor, setBgColor] = useState("");
+  const [bgColor, setBgColor] = useState("ffffff");
   const [qrCode, setQrCode] = useState("");
 
+  // Changing the URL only when the user
+  // changes the input
   useEffect(() => {
-    setQrCode(`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`);
+    setQrCode
+ (`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`);
   }, [word, size, bgColor]);
 
+  // Updating the input word when user
+  // click on the generate button
   function handleClick() {
     setWord(temp);
   }
+
   return (
     <div className="App">
       <h1>QR Code Generator</h1>
-      <div className="inputbox">
-      <div className="gen">
-      <input type="text" onChange={
+      <div className="input-box">
+        <div className="gen">
+          <input type="text" onChange={
             (e) => {setTemp(e.target.value)}}
             placeholder="Enter text to encode" />
-            <button className="button"
-            onclick={handleClick}>Generate QR Code</button>
-      </div>
-      <div className="extra">
+          <button className="button" 
+            onClick={handleClick}>
+            Generate
+          </button>
+        </div>
+        <div className="extra">
           <h5>Background Color:</h5>
           <input type="color" onChange={(e) => 
           { setBgColor(e.target.value.substring(1)) }} />
@@ -36,15 +43,14 @@ function App() {
            value={size} onChange={(e) => 
            {setSize(e.target.value)}} />
         </div>
-        <div className="output-box">
+      </div>
+      <div className="output-box">
         <img src={qrCode} alt="" />
         <a href={qrCode} download="QRCode">
           <button type="button">Download</button>
         </a>
       </div>
-      </div>
     </div>
-
   );
 }
 
